8d65043986d22b5c508fd1ddce743dc476e99ca2
Alexander <maniacallion@gmail.com>
2019-03-25 22:51:05 -0500

    eventlistener added to track image being clicked

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ function Product(name, path) {
 }
 
 
-var bag = new Product('bag.jpg', './img/bag.jpg');
-var banana = new Product('banana.jpg', './img/banana.jpg');
-var bathroom = new Product('bathroom.jpg', './img/bathroom.jpg');
-var boots = new Product('boots.jpg', './img/boots.jpg');
-var breakfast = new Product('breakfast.jpg', './img/breakfast.jpg');
-var bubblegum = new Product('bubblegum.jpg', './img/bubblegum.jpg');
-var chair = new Product('chair.jpg', './img/chair.jpg');
-var cthulhu = new Product('cthulhu.jpg', './img/cthulhu.jpg');
-var dogDuck = new Product('dog-duck.jpg', './img/dog-duck.jpg');
-var dragon = new Product('dragon.jpg', './img/dragon.jpg');
-var pen = new Product('pen.jpg', './img/pen.jpg');
-var petSweep = new Product('pet-sweep.jpg', './img/pet-sweep.jpg');
-var scissors = new Product('scissors.jpg', './img/scissors.jpg');
-var shark = new Product('shark.jpg', './img/shark.jpg');
-var sweep = new Product('sweep.png', './img/sweep.png');
-var tauntaun = new Product('tauntaun.jpg', './img/tauntaun.jpg');
-var usb = new Product('usb.gif', './img/usb.gif');
-var waterCan = new Product('water-can.jpg', './img/water-can.jpg');
-var wineGlass = new Product('wine-glass.jpg', './img/wine-glass.jpg');
+var bag = new Product('bag', './img/bag.jpg');
+var banana = new Product('banana', './img/banana.jpg');
+var bathroom = new Product('bathroom', './img/bathroom.jpg');
+var boots = new Product('boots', './img/boots.jpg');
+var breakfast = new Product('breakfast', './img/breakfast.jpg');
+var bubblegum = new Product('bubblegum', './img/bubblegum.jpg');
+var chair = new Product('chair', './img/chair.jpg');
+var cthulhu = new Product('cthulhu', './img/cthulhu.jpg');
+var dogDuck = new Product('dog-duck', './img/dog-duck.jpg');
+var dragon = new Product('dragon', './img/dragon.jpg');
+var pen = new Product('pen', './img/pen.jpg');
+var petSweep = new Product('pet-sweep', './img/pet-sweep.jpg');
+var scissors = new Product('scissors', './img/scissors.jpg');
+var shark = new Product('shark', './img/shark.jpg');
+var sweep = new Product('sweep', './img/sweep.png');
+var tauntaun = new Product('tauntaun', './img/tauntaun.jpg');
+var usb = new Product('usb', './img/usb.gif');
+var waterCan = new Product('water-can', './img/water-can.jpg');
+var wineGlass = new Product('wine-glass', './img/wine-glass.jpg');
 
 var allProductsArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, usb, waterCan, wineGlass];
 
@@ -58,34 +58,23 @@ var randomProductSet = function () {
 
   var randomProduct1 = productsDonePicked[0];
   document.getElementById('left').src = randomProduct1.path;
+  document.getElementById('left').addEventListener('click', function(){
+    console.log('shit was clicked');
+  });
 
   var randomProduct2 = productsDonePicked[1];
   document.getElementById('middle').src = randomProduct2.path;
+  document.getElementById('middle').addEventListener('click', function(){
+    console.log('shit was clicked');
+  });
 
   var randomProduct3 = productsDonePicked[2];
   document.getElementById('right').src =randomProduct3.path;
+  document.getElementById('right').addEventListener('click', function(){
+    console.log('shit was clicked');
+  });
 
 };
 
 randomProductSet();
 
-/* --------things to attempt to figure out/use -------------
-
-i need to turn all of the products into ojects?
-
-i need to pull a random picture from the allProducts arrary
-Math.random() * allProductsArray.length??
-
-i need to shove 3 different images into the html img tags
-documnet.body.write? ----i don't know
-
-i need to turn them into clickable things and track what gets clicked event listener
-document.getElementById("").addEventListener('click')
-
-i need to get new images when an image is clicked
-
-document.getElementById("left").src <--- something happens with this?
-
-i need to beat my head agaisnt the wall until i don't want to spaz at everyone near me or have a full on anxiety attack
-
-*/
